c5c1c06be0cf5c38a02338d92893f6a3ae3695aa
6704062610178 <s6704062610178@email.kmutnb.ac.th>
2024-11-26 14:57:58 +0700

    Add files via upload

--- lab1minx.c
+++ lab1minx.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 int main(){
-	int var1;
-	int var2;
-	scanf("%d",&var1);
-	scanf("%d",&var2);
+	int var1 = 20;
+	int var2 = 10;
 	if (var1 > var2) {
-		printf("var1 is more than var2 ");
-		printf("\n%d",(var1-var2));
-	} else {
-		printf("var2 is more than var1 ");
-		printf("\n%d",(var2-var1));
-}
+		printf("var1 is more than var2");
+	} else 
+		printf("var2 is more than var1");
 }
