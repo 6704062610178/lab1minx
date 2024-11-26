e6b7268a3ab3c6942a77778dce15fb8773785413
6704062610178 <s6704062610178@email.kmutnb.ac.th>
2024-11-26 15:00:46 +0700

    ข้อ 2 ครับ

--- lab1minx.c
+++ lab1minx.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(){
-	int var1 = 20;
-	int var2 = 10;
+	int var1;
+	int var2;
+	scanf("%d",&var1);
+	scanf("%d",&var2);
 	if (var1 > var2) {
-		printf("var1 is more than var2");
-	} else 
-		printf("var2 is more than var1");
+		printf("var1 is more than var2 ");
+		printf("\n%d",(var1-var2));
+	} else {
+		printf("var2 is more than var1 ");
+		printf("\n%d",(var2-var1));
+}
 }
